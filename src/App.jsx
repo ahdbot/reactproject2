be6daa2f436c2214404179fd61f2a3400eb52383
ahdbot <ahdbot3@gmail.com>
2024-03-17 00:50:57 +0300

    drak mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,76 @@
 import "./App.css";
+import "./theme.css";
+import { useState } from "react";
 
 function App() {
- 
-  let person = "  Ahmad Bajaber ";
+  const [person, setPerson] = useState("Ali Hassan");
+  const [count, setcount] = useState(0);
+  const [name, setChangeName] = useState("Ahmad");
+  const [theme, setTheme] = useState("");
 
-  const ChangeName = () => {
-    console.log("hiiiiiii");
-    person = "  ElRAYEK   ";
-  };
+  // const ChangeName = () => {
+  //   setAge("25");
+  // };
 
   return (
-    <div className="App">
-      <h2>my name is {person}</h2>
-      <button onClick={ChangeName}>Change Name</button>
+    <div className={`App ${theme}`}>
+      <div>
+        <button
+          onClick={() => {
+            setTheme("");
+          }}
+          style={{ marginRight: "26px" }}
+        >
+          {" "}
+          Light
+        </button>
+        <button
+          onClick={() => {
+            setTheme("dark");
+          }}
+          style={{ marginRight: "26px" }}
+        >
+          {" "}
+          Dark
+        </button>
+        <button
+          onClick={() => {
+            setTheme("grey");
+          }}
+          style={{ marginRight: "26px" }}
+        >
+          {" "}
+          Grey
+        </button>
+        <button
+          onClick={() => {
+            setTheme("pink");
+          }}
+        >
+          {" "}
+          Pink{" "}
+        </button>
+      </div>
+      <h2 style={{ marginTop: "22px" }}> My Name is {person}</h2>
 
-      {/* <button> my name is ahmad</button> */}
+      <button
+        onClick={() => {
+          setChangeName("Ali");
+        }}
+      >
+        {" "}
+        Change Name
+      </button>
+      <h2> number : {count} </h2>
+      <button
+        onClick={() => {
+          setcount(1 + count);
+        }}
+      >
+        +
+      </button>
+      <h2>Backgorund color</h2>
+      <button>button</button>
     </div>
   );
 }
